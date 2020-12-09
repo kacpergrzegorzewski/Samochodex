d53c3d9706359d4ce9176c0e2f332a24fd35a4fe
Mateusz Suchocki <suchy.mat@onet.pl>
2020-12-09 17:46:25 +0100

    Update project frameworks

---
+++ src/main/java/edu/bada/samochodex/SamochodexApplication.java
@@ -0,0 +1,13 @@
+package edu.bada.samochodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamochodexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamochodexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bada/samochodex/SamochodexApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bada.samochodex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamochodexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

06ce5a3ef712095d8a2f6ed6fd029c196a4eb42e
Mateusz Suchocki <suchy.mat@onet.pl>
2020-12-12 21:12:12 +0100

    Remove app test

--- src/test/java/edu/bada/samochodex/SamochodexApplicationTests.java
+++ src/test/java/edu/bada/samochodex/SamochodexApplicationTests.java
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SamochodexApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+class SamochodexApplicationTests {}

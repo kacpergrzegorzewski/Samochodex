363226ead5542221c1c14c737650acee52dca026
Kacper Mikołaj Grzegorzewski <k.grzegorzewski@stud.elka.pw.edu.pl>
2021-01-08 14:30:12 +0100

    ShowroomsController.java

--- src/main/java/edu/bada/samochodex/api/ShowroomsController.java
+++ src/main/java/edu/bada/samochodex/api/ShowroomsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/salony")
-public class SalonsController {
+public class ShowroomsController {
 
     @GetMapping
     public String getLoginView() {

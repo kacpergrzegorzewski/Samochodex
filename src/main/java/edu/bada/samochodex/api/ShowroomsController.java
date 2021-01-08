7f3ceaa41bc7d6e03f1906c889206e79e1f97974
Mateusz Suchocki <suchy.mat@onet.pl>
2021-01-08 14:41:07 +0100

    Get car dealers from db

--- src/main/java/edu/bada/samochodex/api/ShowroomsController.java
+++ src/main/java/edu/bada/samochodex/api/ShowroomsController.java
@@ -1,16 +1,33 @@
 package edu.bada.samochodex.api;
 
+import edu.bada.samochodex.model.CarDealer;
+import edu.bada.samochodex.service.CarDealerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/salony")
 public class ShowroomsController {
 
+    private final CarDealerService carDealerService;
+
+    @Autowired
+    public ShowroomsController(CarDealerService carDealerService) {
+        this.carDealerService = carDealerService;
+    }
+
     @GetMapping
-    public String getLoginView() {
-        return "showrooms";
+    public String getCarDealersView(Model model) {
+        List<CarDealer> carDealers = carDealerService.getAll();
+
+        model.addAttribute("carDealers", carDealers);
+
+        return "carDealers";
     }
 
 }
